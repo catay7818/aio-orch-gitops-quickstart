8c968b918aaad8c2fd718584041652702e2ac451
Allen Greaves <allengreaves@microsoft.com>
2024-03-20 11:48:23 -0700

    add providers.tf

---
+++ deploy/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.4.6"
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.11.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.86.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
